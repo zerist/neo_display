2ff9b20b094e9baa92136fc5bdc6780cedbda99e
zerist <15871792739@163.com>
2020-04-13 00:33:54 +0800

    1

--- neo_display/urls.py
+++ neo_display/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('test/', include('display.urls')),
+    path('display/', include('display.urls')),
     path('admin/', admin.site.urls),
 ]
